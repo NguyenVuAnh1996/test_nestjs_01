b8b7783dce3ac6fa64803192f317641ce899f84a
NguyenVuAnh1996 <nguyenvuanh1996@gmail.com>
2024-08-16 09:44:17 +0700

    change to env

--- src/configs/typeorm_configs.ts
+++ src/configs/typeorm_configs.ts
@@ -1,14 +1,17 @@
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DataSource, DataSourceOptions } from 'typeorm';
-import { registerAs } from "@nestjs/config";
+import { config as dotenvConfig } from 'dotenv';
+import { ConfigModule, registerAs } from "@nestjs/config";
+
+dotenvConfig({ path: '.env' });
 
 export const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'Caibanmauxanh@123',
-  database: 'testnestjs01',
+  host: process.env.db_host,
+  port: Number(process.env.db_port),
+  username: process.env.db_username,
+  password: process.env.db_password,
+  database: process.env.db_database,
   entities: [__dirname + '/../**/entities/*.entity{.ts,.js}'],
   migrations: [__dirname + "/../migrations/*{.ts,.js}"],
   migrationsRun: false,
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig({ path: '.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5199, '192.168.1.233');
+  await app.listen(
+    process.env.main_port, 
+    process.env.main_host
+  );
 }
 bootstrap();
