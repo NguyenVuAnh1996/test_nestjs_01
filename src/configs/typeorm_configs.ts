58d81ce2cd10df958bd8ede542c206b1565b4b2b
NguyenVuAnh1996 <85111064+NguyenVuAnh1996@users.noreply.github.com>
2024-12-10 13:44:05 +0700

    quick fix

--- src/configs/typeorm_configs.ts
+++ src/configs/typeorm_configs.ts
@@ -1,24 +1,24 @@
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { config as dotenvConfig } from 'dotenv';
-import { ConfigModule, registerAs } from "@nestjs/config";
+import { registerAs } from '@nestjs/config';
 
 dotenvConfig({ path: '.env' });
 
 export const config: TypeOrmModuleOptions = {
-  type: 'postgres',
+  type: 'mysql',
   host: process.env.db_host,
   port: Number(process.env.db_port),
   username: process.env.db_username,
   password: process.env.db_password,
   database: process.env.db_database,
   entities: [__dirname + '/../**/entities/*.entity{.ts,.js}'],
-  migrations: [__dirname + "/../migrations/*{.ts,.js}"],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
   migrationsRun: false,
   autoLoadEntities: true,
   synchronize: false,
-  ssl: process.env.db_ssl === 'true'
-}
+  // ssl: process.env.db_ssl === 'true',
+};
 
 export const dataSource = registerAs('data-source', () => config);
 
